531bfe5d71eee6ed9fe2271cc43a2c8d93d81a50
hedw1qqq <goga_glushko_igor@mail.ru>
2025-10-03 14:43:59 +0300

    12312132

--- crypto/symmetric_context.py
+++ crypto/symmetric_context.py
@@ -49,47 +49,47 @@ class SymmetricCipherContext:
         await loop.run_in_executor(self._executor, self._decrypt_file_sync, input_path, output_path, chunk_size)
 
     def _encrypt_sync(self, data: bytes) -> bytes:
-        if self.mode in (CipherMode.ECB,):
-            return self._encrypt_ecb(data)
-        if self.mode == CipherMode.CBC:
-            return self._encrypt_cbc(data)
-        if self.mode == CipherMode.PCBC:
-            return self._encrypt_pcbc(data)
-        if self.mode == CipherMode.CFB:
-            return self._encrypt_cfb(data)
-        if self.mode == CipherMode.OFB:
-            return self._encrypt_ofb(data)
-        if self.mode == CipherMode.CTR:
-            return self._encrypt_ctr(data)
-        if self.mode == CipherMode.RANDOM_DELTA:
-            return self._encrypt_random_delta(data)
-        raise NotImplementedError(f"Mode {self.mode} not implemented")
+        match self.mode:
+            case CipherMode.ECB:
+                return self._encrypt_ecb(data)
+            case CipherMode.CBC:
+                return self._encrypt_cbc(data)
+            case CipherMode.PCBC:
+                return self._encrypt_pcbc(data)
+            case CipherMode.CFB:
+                return self._encrypt_cfb(data)
+            case CipherMode.OFB:
+                return self._encrypt_ofb(data)
+            case CipherMode.CTR:
+                return self._encrypt_ctr(data)
+            case CipherMode.RANDOM_DELTA:
+                return self._encrypt_random_delta(data)
+            case _:
+                raise NotImplementedError(f"Mode {self.mode} not implemented")
 
     def _decrypt_sync(self, data: bytes) -> bytes:
-        if self.mode in (CipherMode.ECB,):
-            return self._decrypt_ecb(data)
-        if self.mode == CipherMode.CBC:
-            return self._decrypt_cbc(data)
-        if self.mode == CipherMode.PCBC:
-            return self._decrypt_pcbc(data)
-        if self.mode == CipherMode.CFB:
-            return self._decrypt_cfb(data)
-        if self.mode == CipherMode.OFB:
-            return self._decrypt_ofb(data)
-        if self.mode == CipherMode.CTR:
-            return self._decrypt_ctr(data)
-        if self.mode == CipherMode.RANDOM_DELTA:
-            return self._decrypt_random_delta(data)
-        raise NotImplementedError(f"Mode {self.mode} not implemented")
+        match self.mode:
+            case CipherMode.ECB:
+                return self._decrypt_ecb(data)
+            case CipherMode.CBC:
+                return self._decrypt_cbc(data)
+            case CipherMode.PCBC:
+                return self._decrypt_pcbc(data)
+            case CipherMode.CFB:
+                return self._decrypt_cfb(data)
+            case CipherMode.OFB:
+                return self._decrypt_ofb(data)
+            case CipherMode.CTR:
+                return self._decrypt_ctr(data)
+            case CipherMode.RANDOM_DELTA:
+                return self._decrypt_random_delta(data)
+            case _:
+                raise NotImplementedError(f"Mode {self.mode} not implemented")
 
     def _encrypt_ecb(self, data: bytes) -> bytes:
         padded = pad(data, self.block_size, self.padding)
         blocks = split_blocks(padded, self.block_size)
-        loop = asyncio.get_running_loop()
-        try:
-            results = list(self._executor.map(self.primitive.encrypt_block, blocks))
-        finally:
-            loop.close()
+        results = list(self._executor.map(self.primitive.encrypt_block, blocks))
         return b''.join(results)
 
     def _decrypt_ecb(self, data: bytes) -> bytes:
@@ -112,4 +112,16 @@ class SymmetricCipherContext:
             c = self.primitive.encrypt_block(x)
             out.append(c)
             prev = c
-        return b''.join(out)
+        return iv + b''.join(out)
+
+    def _decrypt_cbc(self, data: bytes) -> bytes:
+        iv = data[:self.block_size]
+        blocks = split_blocks(data[self.block_size:], self.block_size)
+        prev = iv
+        out = []
+        for c in blocks:
+            x = self.primitive.decrypt_block(c)
+            p = xor_bytes(x, prev)
+            out.append(p)
+            prev = c
+        return unpad(b''.join(out), self.block_size, self.padding)
